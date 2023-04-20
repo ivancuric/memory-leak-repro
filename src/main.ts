021ef83897584e3c2bdccefd42410163bac111c5
Ivan Čurić <ivan.curic@gmail.com>
2023-04-20 14:21:15 +0200

    dirty pixel variant

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,30 @@
-const W = 1920;
-const H = 1080;
-
 const canvas = document.createElement("canvas");
-const offscreenCanvas = document.createElement("canvas");
 
-canvas.width = W;
-canvas.height = H;
-
-offscreenCanvas.width = W * 2;
-offscreenCanvas.height = H * 2;
+canvas.width = 1920;
+canvas.height = 1080;
 
 const ctx = canvas.getContext("2d", {
   willReadFrequently: true,
   alpha: false,
 });
 
-const offscreenCtx = offscreenCanvas.getContext("2d", {
-  willReadFrequently: true,
-  alpha: false,
-});
+document.body.appendChild(canvas);
 
-// document.body.appendChild(canvas);
+const imageData = new ImageData(canvas.width, canvas.height);
 
-const imageData = new ImageData(offscreenCanvas.width, offscreenCanvas.height);
-const buffer32 = new Uint32Array(imageData.data.buffer);
+imageData.data.fill(255);
 
-let len = buffer32.length - 1;
-while (len--) buffer32[len] = Math.random() < 0.5 ? 0 : -1 >> 0;
-offscreenCtx!.putImageData(imageData, 0, 0);
+function getRandomArbitrary(min: number, max: number) {
+  return Math.trunc(Math.random() * (max - min) + min);
+}
+
+ctx!.putImageData(imageData, 0, 0);
 
 const drawLoop = () => {
-  const x = Math.trunc(W * Math.random());
-  const y = Math.trunc(H * Math.random());
+  const randomPixel = getRandomArbitrary(0, imageData.data.length - 1);
+  imageData.data[randomPixel] = 0;
 
-  ctx!.drawImage(offscreenCanvas, -x, -y);
+  ctx!.putImageData(imageData, 0, 0);
 
   requestAnimationFrame(drawLoop);
 };
