a59ed651e88b8974eb14a38aae3da8407688f870
Ivan Čurić <ivan.curic@gmail.com>
2023-04-21 12:26:06 +0200

    Created interactive repro sample

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,31 @@
+import { FPS } from "yy-fps";
+
+const fps = new FPS({
+  FPS: 120,
+});
+
+const waitCheckbox = document.querySelector("#wait") as HTMLInputElement;
+const fakeDataCheckbox = document.querySelector(
+  "#fakeimgdata"
+) as HTMLInputElement;
+
+let waitForWorkerResponse = waitCheckbox.checked;
+let useFakeImageData = fakeDataCheckbox.checked;
+
+let animationFrame: number;
+
+waitCheckbox.addEventListener("change", () => {
+  waitForWorkerResponse = waitCheckbox.checked;
+  cancelAnimationFrame(animationFrame);
+  animationFrame = requestAnimationFrame(drawLoop);
+});
+
+fakeDataCheckbox.addEventListener("change", () => {
+  useFakeImageData = fakeDataCheckbox.checked;
+  cancelAnimationFrame(animationFrame);
+  animationFrame = requestAnimationFrame(drawLoop);
+});
+
 const worker = new Worker(new URL("./worker.ts", import.meta.url));
 
 function getRandomArbitrary(min: number, max: number) {
@@ -7,30 +35,40 @@ function getRandomArbitrary(min: number, max: number) {
 let randomPixelLocation: number;
 
 worker.addEventListener("message", (e: MessageEvent<number>) => {
-  console.log(e.data === randomPixelLocation);
-  requestAnimationFrame(drawLoop);
+  const framesInSync = e.data === randomPixelLocation;
+  console.log(framesInSync);
+
+  if (waitForWorkerResponse && framesInSync) {
+    animationFrame = requestAnimationFrame(drawLoop);
+  }
 });
 
-const drawLoop = async () => {
+function drawLoop() {
   if (!worker) {
     return;
   }
 
+  fps.frame();
+
   const imageData = new ImageData(3840, 2160);
 
   randomPixelLocation = getRandomArbitrary(0, imageData.data.length - 1);
-  // console.log(randomPixelLocation);
   imageData.data[randomPixelLocation] = 255;
 
-  worker.postMessage(
-    {
-      data: imageData.data,
-      width: imageData.width,
-      height: imageData.height,
-      colorSpace: imageData.colorSpace,
-    } satisfies ImageData,
-    [imageData.data.buffer]
-  );
-};
+  const fakeImageData = {
+    data: imageData.data,
+    width: imageData.width,
+    height: imageData.height,
+    colorSpace: imageData.colorSpace,
+  } satisfies ImageData;
+
+  const payload = useFakeImageData ? fakeImageData : imageData;
+
+  worker.postMessage(payload, [imageData.data.buffer]);
+
+  if (!waitForWorkerResponse) {
+    animationFrame = requestAnimationFrame(drawLoop);
+  }
+}
 
 requestAnimationFrame(drawLoop);
